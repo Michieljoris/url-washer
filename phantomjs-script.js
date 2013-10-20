34ceefe3556bfe6b959c7700d32aa39d917eac6a
Michiel van Oosten <michieljoris@gmail.com>
2013-10-21 01:27:02 +1000

    First commit

---
+++ phantomjs-script.js
@@ -0,0 +1,75 @@
+/*global phantom:false require:false */
+/*jshint strict:false unused:true smarttabs:true eqeqeq:true immed: true undef:true*/
+/*jshint maxparams:7 maxcomplexity:8 maxlen:150 devel:true newcap:false*/ 
+var page = require('webpage').create();
+page.viewportSize = { width: 1900, height: 1200};
+page.settings.loadImages = false;
+// page.settings.javascriptEnabled = false;
+
+var system = require('system');
+var lastReceived = new Date().getTime() + 100000;
+var requestCount = 0;
+var responseCount = 0;
+var requestIds = [];
+var minResponses = 0;
+var debug = true;
+//TODO use system.env to set some variables, but for now:
+var delay = 1000;
+
+// var initialRequest = null; //not used
+var initialResponse = null;
+var startTime = new Date().getTime();
+
+page.onResourceRequested = function (request) {
+    // console.log('Request (#' + request.id + '): ' + JSON.stringify(request));
+  // initialRequest = initialRequest || request;
+  if(requestIds.indexOf(request.id) === -1) {
+    requestIds.push(request.id);
+    requestCount++;
+  }
+};
+page.onResourceReceived = function (response) {
+  initialResponse = initialResponse || response;
+  if(requestIds.indexOf(response.id) !== -1) {
+      if (responseCount === 1) { // second response
+          minResponses = requestCount;
+      }
+    lastReceived = new Date().getTime();
+    responseCount++;
+    requestIds[requestIds.indexOf(response.id)] = null;
+  }
+};
+
+page.open(system.args[1]);
+
+var checkComplete = function () {
+    var timeDiff = new Date().getTime() - lastReceived;
+    if(timeDiff > delay)
+        if (responseCount >= minResponses &&
+            requestCount === responseCount ||
+            new Date().getTime() - startTime > 30000)  {
+            clearInterval(checkCompleteInterval);
+            console.log(JSON.stringify(initialResponse) + "\n\n");
+            if(initialResponse && initialResponse.contentType === "text/plain") {
+                console.log(page.plainText);
+            } else {
+                console.log(page.content);
+            }
+            phantom.exit();
+        } else {
+            if (debug) {
+                console.error(
+                    'requestCount: ' + requestCount +
+                        ' !== responseCount: ' + responseCount + '.' +
+                        ' \nYou might have a synchronous ajax call that is NOT being captured by onResourceReceived.' +
+                        ' \nSee: https://github.com/ariya/phantomjs/issues/11284' +
+                        ' \nOr the server is just plain not responding to a resource request... ' +
+                        ' \nWhatever the case, this process will time out within 30 seconds, and return what it has anyway'
+                );
+                // console.error('FORCED EXIT STATUS 10. Incomplete in ' + timeDiff/1000 + ' seconds.');
+            }
+            // phantom.exit(10);
+        }
+};
+
+var checkCompleteInterval = setInterval(checkComplete, 100);
---
+++ wash.js
@@ -0,0 +1,169 @@
+/*global module:false __dirname:false module:false require:false */
+/*jshint strict:false unused:true smarttabs:true eqeqeq:true immed: true undef:true*/
+/*jshint maxparams:7 maxcomplexity:8 maxlen:150 devel:true newcap:false*/ 
+
+//Code is based on https://github.com/moviepilot/seoserver which is
+//again based on https://github.com/apiengine/seoserver
+
+//TODO we need a server to talk to, and a script in the browser that does the same as phantom.
+
+var spawn = require('child_process').spawn,
+    Path = require('path'),
+    VOW = require('dougs_vow'),
+    Which = require('which'),
+    request = require('request'),
+    extend = require('extend')
+    ;
+
+var phantomPath;
+
+var log = [];
+
+var options = {
+    verbose: true
+    ,die: 30000
+    // ,phantomPath: 'bla'
+    // ,seoServer: 'http://seoserver.axion5.net'
+};
+
+
+function debug() {
+    if (options.verbose) console.log.apply(console, arguments);
+    log.push(arguments);
+}
+
+try {
+    phantomPath = require('phantomjs').path;
+} catch(e) {
+    debug(e); } 
+
+function initPhantom() {
+    var vow = VOW.make();
+    var path = options.phantomPath || phantomPath || 'phantomjs';
+    Which(path, function(err, path) {
+        // debug(path);
+        if (!err) {
+            vow.keep(path);   
+        }
+        else vow.breek(err);
+    });
+    return vow.promise;
+}
+
+function removeScriptTags(content) {
+    return content.replace(/<script[\s\S]*?<\/script>/gi, '');
+}
+
+function render(url, phantomPath) {
+    var vow = VOW.make(); 
+    var childArgs = [
+        Path.join(__dirname, 'phantomjs-script.js'), url
+    ];
+    var html = "";
+    var err = [];
+    
+    var phantom = spawn(phantomPath, childArgs);
+    
+    var timeout = setTimeout(function() {
+        phantom.kill();
+        vow.breek('Timed out');
+    }, options.die);
+    
+    var headers = {};
+    phantom.stdout.setEncoding('utf8');
+    phantom.stdout.on('data', function (data) {
+        console.log(data);
+        data = data.toString();
+        var responseHeaders;
+        var match = data.match(/(\{.*?\})\n\n/);
+        if (match) {
+            responseHeaders = JSON.parse(match[1]);
+            if (responseHeaders.status) {
+                headers.status = responseHeaders.status;
+            }
+            if (responseHeaders.status === 301) {
+                headers.location = responseHeaders.redirectURL;
+            }
+            headers.contentType = responseHeaders.contentType;
+            data = data.replace(/(.*?)\n\n/, '');
+            // debug(responseHeaders);
+        }
+        if (data.match(/^\w*error/i)) {
+            headers.status = 503;
+            debug("js error: " + data.toString());
+        }
+        else html += data;
+    });
+
+    phantom.stderr.on('data', function (data) {
+        err.push(data.toString());
+    });
+
+    // phantom.on('close', function (code) {
+    //     if (code) {
+    //         vow.breek(code);
+    //         return;
+    //     }
+    //     vow.keep({ html: html, err: err });
+    // });
+    
+    phantom.on('exit', function(code) {
+        clearTimeout(timeout);
+        if (code) {
+            debug('Error on PhantomJS process');
+            vow.breek(code);
+        } else {
+            //  We chose to remove all script tags,
+            //  otherwise if/when google bot will start to parse js
+            //  it will lead to duplicate renderings of the page.
+            vow.keep({ html: removeScriptTags(html), err: err });
+        }
+    });
+    
+    return vow.promise;   
+}
+
+function wash(url, someOptions) {
+    var vow = VOW.make();
+    options = extend(options, someOptions);
+    initPhantom()
+        .when(
+            function(phantomPath) {
+                return render(url, phantomPath);
+            })
+        .when(
+            function(result) {
+                // debug(result.html, result.err);
+                vow.keep(result.html);
+            }
+            ,function(err) {
+                if (options.seoServer) {
+                    debug("Not able to use native phantomjs. Trying options.seoServer", err);
+                    request({ uri: options.seoServer
+                              ,qs: {
+                                  url: url
+                              }
+                            }, function (error, response, body) {
+                                if (!error && response.statusCode === 200) {
+                                    vow.keep(body);
+                                }
+                                else {
+                                    vow.breek('Not able to render ' + url + ' code: ' + error.toString());   
+                                }
+                            });
+                } 
+                else {
+                    vow.breek('Not able to render ' + url + ' code: ' + err);   
+                }
+            }
+        );
+    return vow.promise;
+}
+
+module.exports = wash;
+
+//testing:
+// wash('http://firstdoor.axion5.net',{ phantomPath: 'phantomjs' }).when(
+//     function(data) { console.log(data);}
+//     ,function(data) { console.log('error', data);}
+// );
